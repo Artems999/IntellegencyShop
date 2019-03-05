389074edd64fb684c67ff7bbeb5d798de57d668c
Babichartem <Babichartemson@gmail.com>
2019-03-05 23:12:09 +0300

    It's work!

--- SmartShop/IntelligenceShop.cs
+++ SmartShop/IntelligenceShop.cs
@@ -14,15 +14,22 @@ namespace SmartShop
 
         public List<TradeItem> TradeItemList { get; set; }
 
+        public IntelligenceShop()
+        {
+            CreateBuyersList();
+            CreateTradeItemsList();
+        }
 
         public void CreateBuyersList()
         {
+            BuyersList = new List<Buyer>();
             BuyersList.Add(new Buyer(0, "Arkadiy", "Semenov", 25, Worker.WorkPlace.Developer, 8000));
             BuyersList.Add(new Buyer(1, "Evgeniy", "Smirnov", 25, Worker.WorkPlace.SportTrainer, 5000));
             BuyersList.Add(new Buyer(2, "Dmitry", "Olenichev", 25, Worker.WorkPlace.Designer, 9000));
         }
         public void CreateTradeItemsList()
         {
+            TradeItemList = new List<TradeItem>();
             TradeItemList.Add(new TradeItem(0, "Milk", 60, TradeItem.ProductCategory.Products));
             TradeItemList.Add(new TradeItem(1, "Protein", 600, TradeItem.ProductCategory.SportEat));
             TradeItemList.Add(new TradeItem(2, "Curtains", 6000, TradeItem.ProductCategory.InteriorItems));
@@ -30,26 +37,44 @@ namespace SmartShop
             TradeItemList.Add(new TradeItem(4, "Total Commander", 60, TradeItem.ProductCategory.Software));
         }
 
-      /*  public List<TradeItem> CalculateItems(Buyer buyer, List<TradeItem> tradeItem)
+        public List<TradeItem> CalculateItems(Buyer buyer, List<TradeItem> tradeItem)
         {
             List<TradeItem> calculatedItems = new List<TradeItem>();
             foreach (var item in tradeItem)
             {
-                switch (buyer.PersonWorkPlace)
+                if (buyer.BuyerCash > item.Price)
                 {
-                    case Worker.WorkPlace.Developer:
-                        break;
-                    case Worker.WorkPlace.Designer:
-                        break;
-                    case Worker.WorkPlace.SportTrainer:
-                        break;
-                    default:
-                        break;
+                    switch (item.ProdCategory)
+                    {
+                        case TradeItem.ProductCategory.SportEat:
+                            if (buyer.PersonWorkPlace == Buyer.WorkPlace.SportTrainer)
+                            {
+                                calculatedItems.Add(item);
+                            }
+                            break;
+                        case TradeItem.ProductCategory.Products:
+                            calculatedItems.Add(item);
+                            break;
+                        case TradeItem.ProductCategory.InteriorItems:
+                            if (buyer.PersonWorkPlace == Buyer.WorkPlace.Designer)
+                            {
+                                calculatedItems.Add(item);
+                            }
+                            break;
+                        case TradeItem.ProductCategory.Software:
+                            if (buyer.PersonWorkPlace == Buyer.WorkPlace.Developer)
+                            {
+                                calculatedItems.Add(item);
+                            }
+                            break;
+                        default:
+                            break;
+                    }
                 }
             }
             return calculatedItems;
         }
-        */
+        
 
 
 
--- SmartShop/Program.cs
+++ SmartShop/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SmartShop.Peoples;
+using SmartShop.Trade;
 
 namespace SmartShop
 {
@@ -12,10 +13,20 @@ namespace SmartShop
         
         static void Main(string[] args)
         {
-            Buyer myBuyer = new Buyer(0, "Arkadiy","Samsonov", 25,Worker.WorkPlace.Developer,7000);
+            IntelligenceShop intelligenceShop = new IntelligenceShop();
 
+            foreach (var buer in intelligenceShop.BuyersList)
+            {
+                List<TradeItem> calculated = intelligenceShop.CalculateItems(buer, intelligenceShop.TradeItemList);
+                Console.Write("{0} {1} works as a {2} and can buy: ", buer.FirstName, buer.LastName,buer.PersonWorkPlace.ToString());
+                foreach (var tradeItem in calculated)
+                {
+                    Console.Write("{0} || ", tradeItem.ItemName);
+                }
+                Console.WriteLine();
+            }
+            
 
-            Console.WriteLine("FirstName: {0} LastName: {1} Age: {2} WorkPlace: {3}", myBuyer.FirstName, myBuyer.LastName, myBuyer.PersonAge, myBuyer.PersonWorkPlace.ToString());
             Console.ReadLine();
         }
     }
